b4ad3d82f759e7af9ce7b314d44b04671c214c3c
Sander Birkhol <sanderbirkhol@gmail.com>
2026-07-03 11:04:50 +0200

    feat: Notes section with local storage

--- my-dashboard/src/widgets/Notes/Notes.tsx
+++ my-dashboard/src/widgets/Notes/Notes.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react"
 import "./Notes.css"
 
 function Notes() {
+    const [note, setNote] = useState(() => {
+        const savedNote = localStorage.getItem("note")
+
+        if (savedNote === null) {
+            return ""
+        }
+
+        return savedNote
+    })
+
+    useEffect(() => {
+        localStorage.setItem("note", note)
+    }, [note])
     return (
         <section className="widget notes">
             <h2>Notes</h2>
+            <textarea value={note} onChange={event => setNote(event.target.value)} placeholder="Write your notes here..." />
         </section>
     )
 }
