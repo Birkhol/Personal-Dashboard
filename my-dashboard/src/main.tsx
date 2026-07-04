e0f7638452d2e52556707982bd0f65457952522e
Sander Birkhol <sanderbirkhol@gmail.com>
2026-07-04 17:40:04 +0200

    fix: Redirects to correct domain

--- my-dashboard/src/main.tsx
+++ my-dashboard/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (window.location.hostname === "my-dashboard-1qh.pages.dev") {
+  window.location.replace("https://sander-dashboard.no")
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
