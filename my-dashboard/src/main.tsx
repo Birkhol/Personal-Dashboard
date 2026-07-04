efd6a2ef4a324c9c25fad450772359551c0233ba
Sander Birkhol <sanderbirkhol@gmail.com>
2026-07-04 18:17:43 +0200

    fix: Redirect domain

--- my-dashboard/src/main.tsx
+++ my-dashboard/src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-if (window.location.hostname === "my-dashboard-1qh.pages.dev") {
+const redirectUrl = import.meta.env.DOMAIN_REDIRECT_URL
+
+if (window.location.hostname === redirectUrl) {
   window.location.replace("https://sander-dashboard.no")
 }
 
