cea9bf520e1ae14c3429180727cd9fae271b1c3d
Sander Birkhol <sanderbirkhol@gmail.com>
2026-07-04 14:23:44 +0200

    fix: Changed replacestate to work better with deployment

--- my-dashboard/src/services/spotifyService.ts
+++ my-dashboard/src/services/spotifyService.ts
@@ -64,7 +64,7 @@ export async function handleSpotifyCallback() {
         return
     }
 
-    window.history.replaceState({}, document.title, "/")
+    window.history.replaceState({}, "", window.location.pathname)
 
     const codeVerifier = localStorage.getItem("spotify_code_verifier")
 
